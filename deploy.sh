8b88264f790caf96b78b78015dcb5a38fefecfe9
Scionwest <scionwest+github@gmail.com>
2020-08-23 20:34:07 +0000

    Fixed deployment script not naming stacks consistently

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo Deploying into the $deployed_environment environment.
 npm install
 
 # Execute the SAM CLI Deploy command to upload the Lambdas to S3 and deploy them
-sam_stack_name=focusmark-"$deployed_environment"-sam-api-task
+sam_stack_name=focusmark-"$deployed_environment"-sam-api-taskserverless
 sam_template_file='template.sam'
 sam_s3_bucket_name=focusmark-$deployed_environment-s3-deployments
 
@@ -21,7 +21,7 @@ sam deploy \
   
 # Execute the CloudFormation template needed to map the API Gateway associated with the above SAM tempalte
 # to the existing custom domain deployed with the core infrastructure
-cf_stack_name=focusmark-"$deployed_environment"-cf-apiTaskDomainMapping
+cf_stack_name=focusmark-"$deployed_environment"-cf-api-taskroute
 cf_template_file='domain-mapping.yaml'
 
 echo Deploying the $cf_stack_name stack.
