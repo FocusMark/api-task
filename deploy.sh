85d18d9533687532be731f4dc68989af383c181f
Scionwest <scionwest+github@gmail.com>
2020-11-25 06:10:58 +0000

    Removed dns mapping. Moved to aws-dns repo

--- deploy.sh
+++ deploy.sh
@@ -19,18 +19,3 @@ sam deploy \
   --parameter-overrides \
       TargetEnvironment=$deployed_environment \
       ProductName=$product_name
-  
-# Execute the CloudFormation template needed to map the API Gateway associated with the above SAM tempalte
-# to the existing custom domain deployed with the core infrastructure
-cf_stack_name=focusmark-"$deployed_environment"-cf-api-taskroute
-cf_template_file='domain-mapping.yaml'
-
-echo Deploying the $cf_stack_name stack.
-cfn-lint $cf_template_file
-aws cloudformation deploy \
-  --template-file $cf_template_file \
-  --stack-name $cf_stack_name \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-      TargetEnvironment=$deployed_environment \
-      ProductName=$product_name
